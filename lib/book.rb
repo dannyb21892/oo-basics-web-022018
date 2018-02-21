c3c1cde60b487e13c8e268038aedacaa7a0cd9fc
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-21 19:25:18 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,10 +2,13 @@ class Book
   def initialize(name)
     @title = name
   end
+  
    def turn_page
      puts "Flipping the page...wow, you read fast!"
    end
    
    attr_accessor :author, :page_count, :genre
+   
+   attr_reader :title
  end
 
