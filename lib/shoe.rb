ad67ee3067854e29b103a447f4712a3d9db5a602
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-21 19:27:16 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Book
+  def initialize(brand)
+    @brand = brand
+  end
+  
+   def cobble
+     puts "The shoe has been repaired"
+     @condition = "new"
+   end
+   
+   attr_accessor :color, :size, :material, :condition
+   
+   attr_reader :brand
+end
